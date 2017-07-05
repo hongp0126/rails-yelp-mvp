73e65f4c51be1c6a268135093155394a29af7c7c
Hong Park <hongp26@gmail.com>
2017-07-05 15:58:04 -0400

    finish rails yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Montreal",
+    category:     "french",
+    phone_number: "438-947-1234"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Montreal",
+    category:     "italian",
+    phone_number: "438-356-1674"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "chinese",
+    phone_number: "438-947-1234"
+  },
+  {
+    name:         "Kuu",
+    address:      "145 rue Saint-Catherine Montreal",
+    category:     "japanese",
+    phone_number: "514-023-4567"
+  },
+  {
+    name:         "4 saison",
+    address:      "869 rue Saint-Laurent Montreal",
+    category:     "belgian",
+    phone_number: "514-346-0987"
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
